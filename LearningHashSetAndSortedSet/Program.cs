fd7a7e842bcb856d6558d8d6ea2a2291b55d5b33
Yago Gama de Araújo <yagoggb97@gmail.com>
2022-11-14 17:39:10 -0300

    Using HashSet and SortedSet commands

--- LearningHashSetAndSortedSet/Program.cs
+++ LearningHashSetAndSortedSet/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
 namespace LearningHashSetAndSortedSet
 {
@@ -7,15 +8,29 @@ namespace LearningHashSetAndSortedSet
     {
         static void Main(string[] args)
         {
-            SortedSet<int> a = new SortedSet<int> {1, 3, 5, 7, 9, 11, };
-            SortedSet<int> b = new SortedSet<int> { 2, 4, 6, 8, 10, 12 };
+            SortedSet<int> a = new SortedSet<int> {1, 2, 3, 4, 5, 7, 9, 11, };
+            SortedSet<int> b = new SortedSet<int> { 2, 4, 6, 8, 9, 10, 12, 13 };
 
-            a.UnionWith(b);
-            foreach(int n in a)
+            a.UnionWith(b); //Union
+            PrintCollection(a);
 
+            SortedSet<int> c = new SortedSet<int>(a);
+            c.IntersectWith(b); //Intersection
+            PrintCollection(c);
+
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.ExceptWith(b); //Difference
+            PrintCollection(d);
+
+
+            static void PrintCollection<T>(IEnumerable<T> Collection)
             {
-                Console.Write(n);
-                Console.Write(" ");
+                foreach (T obj in Collection)
+                {
+                    Console.Write(obj + ", ");
+                }
+
+                Console.WriteLine();
             }
            
         }
