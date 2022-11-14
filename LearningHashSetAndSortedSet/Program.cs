cecbe74ca9ed6fb19bc125da9db0528206545757
Yago Gama de Araújo <yagoggb97@gmail.com>
2022-11-14 17:30:26 -0300

    Using SortedSet

--- LearningHashSetAndSortedSet/Program.cs
+++ LearningHashSetAndSortedSet/Program.cs
@@ -7,8 +7,8 @@ namespace LearningHashSetAndSortedSet
     {
         static void Main(string[] args)
         {
-            HashSet<int> a = new HashSet<int> {1, 3, 5, 7, 9, 11, };
-            HashSet<int> b = new HashSet<int> { 2, 4, 6, 8, 10, 12 };
+            SortedSet<int> a = new SortedSet<int> {1, 3, 5, 7, 9, 11, };
+            SortedSet<int> b = new SortedSet<int> { 2, 4, 6, 8, 10, 12 };
 
             a.UnionWith(b);
             foreach(int n in a)
