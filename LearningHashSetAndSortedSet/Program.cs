7e084aa2156dc504dae6d6e874eda321adea92fb
Yago Gama de Araújo <yagoggb97@gmail.com>
2022-11-14 17:21:58 -0300

    Using HashSet

---
+++ LearningHashSetAndSortedSet/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningHashSetAndSortedSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> a = new HashSet<int> {1, 3, 5, 7, 9, 11, };
+            HashSet<int> b = new HashSet<int> { 2, 4, 6, 8, 10, 12 };
+
+            a.UnionWith(b);
+            foreach(int n in a)
+
+            {
+                Console.Write(n);
+                Console.Write(" ");
+            }
+           
+        }
+    }
+}
